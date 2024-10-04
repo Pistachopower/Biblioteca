55f993920c3df1f64a018947af305ff099a26a02
Pistachopower <nelson.galicia.carrero.al@iespoligonosur.org>
2024-10-04 20:36:28 +0200

    Se arregla error de sintaxis con los atributos de los modelos

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -7,10 +7,10 @@ from django.utils import timezone
 
 class Libro(models.Model):
     id_libro = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    titulo= text = models.TextField()
+    titulo= models.TextField()
     id_autor = models.CharField(max_length=200)
 
 class Autores(models.Model):
     id_autor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    nombre= text = models.TextField()
+    nombre= models.TextField()
     fecha_prestamo = models.DateTimeField(default=timezone.now)
