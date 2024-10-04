8dace0a68939c64a68ead198b59e7c59382f3e6f
Pistachopower <nelson.galicia.carrero.al@iespoligonosur.org>
2024-10-04 23:33:08 +0200

    pagina funcional

--- biblioteca/views.py
+++ biblioteca/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
+from .models import Libro, Autores
 
 # Por cada modelo creamos una vista
 def libro_list(request):
-    
-    return render(request, 'biblioteca/libro_list.html',{})
+    libro = Libro.objects.all()
+    return render(request, 'biblioteca/libro_list.html',{'libro_list':libro})
 
 
 def autores_list(request):
-    return render(request, 'biblioteca/autores_list.html',{})
+    autores= Autores.objects.all()
+    return render(request, 'biblioteca/autores_list.html',{'autores_list':autores})
 
 
